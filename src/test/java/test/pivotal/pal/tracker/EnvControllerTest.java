73fdd7eb0119cce74a456ee88e836c7a7ff00f55
koziol <lukasz.koziol@emc.com>
2019-08-26 11:24:27 +0200

    1

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -8,8 +8,10 @@ import io.pivotal.pal.tracker.EnvController;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EnvControllerTest {
+
     @Test
     public void getEnv() throws Exception {
+
         EnvController controller = new EnvController(
             "8675",
             "12G",
